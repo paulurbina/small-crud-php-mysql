b5ac896f8ed39fa819c9cdfb806d9135dc44f52d
tonypera <paulurbina@hotmail.com>
2019-01-04 15:56:00 -0500

    Added a Procfile

---
+++ web/db.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+
+    $conn = mysqli_connect(
+        "localhost",
+        "paul",
+        "Ny07JIVp9x3FcmcX",
+        "php_mysql_crud"
+    );
+
+?>
---
+++ web/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP MYSQL CRUD</title>
+    <!--BOOSTRAP 4-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!--FONT AWESOME 5-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </nav>
+    
